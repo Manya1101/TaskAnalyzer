c791689234b5df418428810c985482f346b725a5
Manya Aggarwal <manyaaggarwal1103@gmail.com>
2025-11-27 23:14:45 +0530

    Add Task model

---
+++ backend/tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Task(models.Model):
+title = models.CharField(max_length=255)
+due_date = models.DateField(null=True, blank=True)
+estimated_hours = models.FloatField(default=1)
+importance = models.IntegerField(default=5)
+# dependencies: tasks this task depends on
+dependencies = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='dependents')
+
+
+def __str__(self):
+return self.title
